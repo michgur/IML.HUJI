09839b84370527a85669bf5281b68f1b8592da31
michgur <michael@thegur.net>
2023-05-24 23:23:28 +0300

    cross validate impl

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -6,7 +6,7 @@ from IMLearn import BaseEstimator
 
 
 def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
-                   scoring: Callable[[np.ndarray, np.ndarray, ...], float], cv: int = 5) -> Tuple[float, float]:
+                   scoring: Callable[[np.ndarray, np.ndarray], float], cv: int = 5) -> Tuple[float, float]:
     """
     Evaluate metric by cross-validation for given estimator
 
@@ -37,4 +37,18 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    train_e, validation_e = np.zeros(cv), np.zeros(cv)
+    # indices to split data into train and validation sets
+    cv_indices = np.arange(0, X.shape[0], X.shape[0] // cv)
+    for i in range(cv):
+        # train on all data except S_i
+        train_X = np.concatenate((X[:cv_indices[i]], X[cv_indices[i + 1]:]))
+        train_y = np.concatenate((y[:cv_indices[i]], y[cv_indices[i + 1]:]))
+        validation_X = X[cv_indices[i]:cv_indices[i + 1]]
+        validation_y = y[cv_indices[i]:cv_indices[i + 1]]
+
+        # fit estimator on train data
+        h = deepcopy(estimator).fit(train_X, train_y)
+        train_e[i] = scoring(train_y, h.predict(train_X))
+        validation_e[i] = scoring(validation_y, h.predict(validation_X))
+    return np.mean(train_e), np.mean(validation_e)
